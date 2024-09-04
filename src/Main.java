7e4236768b0fc2957b90e6be5d8503ef3c995c51
dtucker <davidt2003@msn.com>
2024-09-04 13:35:35 -0400

    feat: prepared game window

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        GameWindow game = new GameWindow();
+        game.start();
     }
 }
