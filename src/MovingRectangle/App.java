5afd5bf6cc1e0251b7d78d10e0414dcd1b515f63
dtucker <davidt2003@msn.com>
2024-09-18 13:48:28 -0400

    feat: amazing new game

---
+++ src/MovingRectangle/App.java
@@ -0,0 +1,11 @@
+package MovingRectangle;
+
+import Doctrina.Game;
+
+public class App {
+
+    public static void main(String[] args) {
+        Game game = new MovingRectangleGame();
+        game.start();
+    }
+}
---
+++ src/MovingRectangle/MovingRectangleGame.java
@@ -0,0 +1,22 @@
+package MovingRectangle;
+
+import Doctrina.Canvas;
+import Doctrina.Game;
+
+public class MovingRectangleGame extends Game {
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void update() {
+
+    }
+
+    @Override
+    protected void draw(Canvas canvas) {
+
+    }
+}
