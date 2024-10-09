baae791c6a497a1a37e6e0be96d04100f197a298
jordanrioux <jordan.riouxleclair@gmail.com>
2024-10-09 13:09:04 -0400

    feat: prepare missile

--- src/Tank/Missile.java
+++ src/Tank/Missile.java
@@ -10,16 +10,43 @@ public class Missile extends MovableEntity {
     private final Direction tankDirection;
 
     public Missile(Tank tank) {
-        setDimension(5, 5);
-        setSpeed(5);
-        teleport(tank.getX(), tank.getY());
-        
         tankDirection = tank.getDirection();
+        initialize(tank);
+    }
+
+    private void initialize(Tank tank) {
+        setSpeed(5);
+
+        if (tankDirection == Direction.RIGHT) {
+            setDimension(8, 4);
+
+            // x = tankX + tankWidth
+            // y = tankY + tankHeight/2 + missileHeight/2
+            teleport(
+                    tank.getX() + tank.getWidth() + 1,
+                    tank.getY() + tank.getHeight()/2 - height/2
+            );
+        } else if (tankDirection == Direction.LEFT) {
+            setDimension(8, 4);
+            teleport(tank.getX() - 9, tank.getY() + 15 - 2);
+        } else if (tankDirection == Direction.DOWN) {
+            setDimension(4, 8);
+            teleport(tank.getX() + 15 - 2, tank.getY() + 30 + 1);
+        }  else if (tankDirection == Direction.UP) {
+            setDimension(4, 8);
+            teleport(tank.getX() + 15 - 2, tank.getY() - 9);
+        }
     }
 
     @Override
     public void update() {
         move(tankDirection);
+        if (x >= 820) {
+            x = -20;
+        }
+        if (y >= 620) {
+            y = -20;
+        }
     }
 
     @Override
